02f7ddd3c53a6b350c3cc4d34dc3e0591f816e45
songsick <songsig2@naver.com>
2019-10-05 18:24:01 +0900

    redux 적용하기 2 - App 컴포넌트의 state 변수를 store에 저장하고 props로 내려받기

--- src/App.js
+++ src/App.js
@@ -3,24 +3,25 @@ import './App.css';
 import {Header} from "./Header";
 import {Player} from "./Player";
 import {AddPlayerForm} from "./AddPlayerForm";
+import {connect} from "react-redux";
 
 class App extends React.Component {
 	maxId=4;//class 속성, 접근할시 this. 붙일것
-  state = {
+ /* props = {
     players: [
       {name: 'LDK', score: 30, id: 1},
       {name: 'HONG', score: 40, id: 2},
       {name: 'KIM', score: 50, id: 3},
       {name: 'PARK', score: 60, id: 4}
     ]
-  } //class 속성
+  } //class 속성*/
   render() {
     return (
       <div className="scoreboard">
-        <Header title="My Scoreboard" players={this.state.players} />
+        <Header title="My Scoreboard" players={this.props.players} />
 
         {
-          this.state.players.map((player) => {
+          this.props.players.map((player) => {
             return (
               <Player name={player.name} score={player.score} id={player.id} key={player.id}
                 // 2. props로 콜백 펑션을 전달
@@ -64,7 +65,7 @@ class App extends React.Component {
 			}*/
 
   		const players=[...prevState.players];//원본배열 훼손 x
-			players.push({/*name:name*/name,id:++this.maxId,score:0});
+			players.push({name/*name:name*/,id:++this.maxId,score:0});
 			return{
 				//players:players
 				players
@@ -73,4 +74,9 @@ class App extends React.Component {
 	}
 }
 
-export default App;
+const mapStateToPtops =(state) =>({
+	//왼쪽은 props,오른쪽이 store state
+	players:state.playerReducer.players
+})
+
+export default connect(mapStateToPtops)(App);
