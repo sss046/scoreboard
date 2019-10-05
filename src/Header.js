22f8cb793a795d4c21374da276687ad9f4abde50
songsick <songsig2@naver.com>
2019-10-05 13:16:16 +0900

    Header에 객체할당 문법 적용

--- src/Header.js
+++ src/Header.js
@@ -4,12 +4,11 @@ import {Stopwatch} from "./Stopwatch";
 // function component 만드는 법
 // 1. 첫글자가 대문자
 // 2. React Elemement를 리턴
-export const Header = (props) => {
-	console.log(props);
+export const Header = ({players,title}) => {
 	return (
 		<header className="header">
-			<Stats players={props.players} />
-			<h1 className="h1">{props.title}</h1>
+			<Stats players={players} />
+			<h1 className="h1">{title}</h1>
 			<Stopwatch/>
 		</header>
 	);
