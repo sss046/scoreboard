3c51fe85e499bfb2b9cbe41e0217ac3da5960635
songsick <songsig2@naver.com>
2019-10-05 13:16:53 +0900

    Player에 객체해체 문법 적용

--- src/Player.js
+++ src/Player.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import {Counter} from "./Counter";
 
-export class Player extends React.PureComponent{
+export class Player extends React.Component{
 		render() {
-		console.log(this.props.name, ' rendered');
+		const {removePlayer,id,name,score,changeScore}=this.props;
 		return(
 		<div className="player">
 		<span className="player-name">
 			{/*3. 콜백 펑션 호출*/}
-			<button className="remove-player" onClick={() => this.props.removePlayer(this.props.id)}> X </button>
-			{this.props.name}
+			<button className="remove-player" onClick={() => removePlayer(id)}> X </button>
+			{name}
 		</span>
-			<Counter score={this.props.score} changeScore={this.props.changeScore} id={this.props.id} />
+			<Counter score={score} changeScore={changeScore} id={id} />
 		</div>
 	);
+	}
+	shouldComponentUpdate(nextProps, nextState, nextContext) {
+			//shallow compar...
+			return nextProps.score !== this.props.score? true:false;
 	}
 }
